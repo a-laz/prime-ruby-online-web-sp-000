64d6c8a8662922bafcb6062159c615c95e85002d
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-08 05:05:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  #binding.pry
+  if number == 1
+    false
+  elsif number < 0
+    false
+  else
+    divisor = 2
+    while divisor * divisor <= number
+      return false if number % divisor == 0
+      divisor += 1
+    end
+  end
+  true
+end
